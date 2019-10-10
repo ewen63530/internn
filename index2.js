ae4d836870106c6548b1c8b94a6ba3a4b9a47f53
ewen63530 <47788650+ewen63530@users.noreply.github.com>
2019-10-10 08:33:10 +0200

    Rename index.js to index2.js

---
+++ index2.js
@@ -0,0 +1,16 @@
+//base
+const Discord = require('discord.js'); 
+ const client = new Discord.Client(); var prefix = "%"; 
+ 
+client.login('token');
+
+
+client.on('message',message=>{
+if(message content === "?help" 
+let test = new Discord.Richembed()
+.setTitle(**Help**)
+.setURL('URL)
+.addfield('TITRE DE LA CATHEGORIE' , 'texte \n texte \n texte \n texte ') 
+.setThumbeil('Url de l'image ou du gif) 
+.setimage('url de l'image) 
+message.channel.send(test)
